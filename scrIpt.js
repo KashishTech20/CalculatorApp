a96b7d0e0fe535c45c92d9ffceb05b09dad28a57
Kashish Saini <kashishsaini2027@gmail.com>
2026-06-23 22:29:33 +0530

    Update scrIpt.js

--- scrIpt.js
+++ scrIpt.js
@@ -1,28 +1,21 @@
  const display = document.getElementById("display");
 const buttons = document.querySelectorAll(".btn");
 
-buttons.forEach(button => {
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
 
-    button.addEventListener("click", () => {
-
-        const value = button.innerText;
-
-        if(value === "AC"){
-            display.value = "";
-        }
-
-        else if(value === "="){
-            try{
-                display.value = eval(display.value);
-            }
-            catch{
-                display.value = "Error";
-            }
-        }
-
-        else{
-            display.value += value;
-        }
-    });
-
+    if (value === "AC") {
+      display.value = "";
+    } else if (value === "=") {
+      try {
+        display.value = Function("return " + display.value)();
+      } catch (error) {
+        display.value = "Error";
+      }
+    } else {
+      display.value += value;
+    }
+  });
 });
+ 
